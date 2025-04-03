19978e66b7a695218307b27605ea11cfacc16495
DeborahAdedigba <145978156+DeborahAdedigba@users.noreply.github.com>
2025-04-03 05:37:10 +0100

    Update Soli_app.py

--- Testing/Soli_app.py
+++ Testing/Soli_app.py
@@ -2085,7 +2085,8 @@ def main():
                 determine_best_time_to_trade_future(coin, days)
             else:
                 model = st.selectbox("Select model:", ["SVR", "GBR", "XGBoost", "LSTM"])
-                determine_best_time_to_trade(coin, days, model)
+                # determine_best_time_to_trade(coin, days, model)
+                create_prediction_interface() 
         elif prediction_option == "Predict coin by Profit":
             model_type = st.selectbox("Select model:", ['Gradient_Boosting', 'SVR', 'Xgboost', 'LSTM'])
             profit = st.number_input("Desired profit:", value=100)
